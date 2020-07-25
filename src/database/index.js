f06fdd3f6ef01e8f9be692c3308334c45900fc92
brenoaraujo1 <brenoaraujo@outlook.com.br>
2020-07-25 00:57:18 -0300

    CarUser init e associate

--- src/database/index.js
+++ src/database/index.js
@@ -3,12 +3,16 @@ const dbConfig = require('../config/database');
 
 const Car = require('../app/models/Car');
 const Driver = require('../app/models/Driver');
+const CarUse = require('../app/models/CarUse');
 
 
 const connection = new Sequelize(dbConfig);
 
 Car.init(connection);
 Driver.init(connection);
+CarUse.init(connection);
+
+CarUse.associate(connection.models);
 
 
 module.exports = connection;
