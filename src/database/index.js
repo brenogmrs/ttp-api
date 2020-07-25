73ae4f5c8f4c120ecb760d45e08917f01af54191
brenoaraujo1 <brenoaraujo@outlook.com.br>
2020-07-25 00:20:29 -0300

    iniciando models do sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const Car = require('../app/models/Car');
+const Driver = require('../app/models/Driver');
+
 
 const connection = new Sequelize(dbConfig);
 
 Car.init(connection);
+Driver.init(connection);
+
 
 module.exports = connection;
